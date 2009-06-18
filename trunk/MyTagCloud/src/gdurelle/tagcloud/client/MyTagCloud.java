53886c49943f86743ea81838e2661c8bce8615b1
gregory.durelle <gregory.durelle@127e1f52-4ed4-11de-9264-43ebd13e77c3>
2009-06-18 20:49:16 +0000

    changing an int to double in the TagCloud, thus calculations are more flexible

--- trunk/MyTagCloud/src/gdurelle/tagcloud/client/MyTagCloud.java
+++ trunk/MyTagCloud/src/gdurelle/tagcloud/client/MyTagCloud.java
@@ -2,10 +2,12 @@ package gdurelle.tagcloud.client;
 
 import gdurelle.tagcloud.client.tags.ImageTag;
 import gdurelle.tagcloud.client.tags.TagCloud;
+import gdurelle.tagcloud.client.tags.WordTag;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -13,18 +15,39 @@ import com.google.gwt.user.client.ui.RootPanel;
 public class MyTagCloud implements EntryPoint {
 
     public void onModuleLoad() {
+
+    	VerticalPanel panel = new VerticalPanel();
+        panel.setWidth("200px");
+    	
         TagCloud cloud = new TagCloud();
         cloud.setColored(true);
+        cloud.setWidth("200px");
         cloud.populate();
-        cloud.setWidth("400px");
-        RootPanel.get("content").add(cloud);
-
+        
         TagCloud photoCloud = new TagCloud();
         Image im = new Image("/pdf.PNG");
         photoCloud.addImage(new ImageTag(im));
         photoCloud.addImage(new ImageTag("/odt.PNG"));
         photoCloud.addImage(new ImageTag("http://media.linkedin.com/mpr/mpr/shrink_80_80/p/3/000/01c/01d/2192909.jpg"));
-        photoCloud.setWidth("20px");
-        RootPanel.get("content").add(photoCloud);
+        
+        TagCloud mini = new TagCloud();
+        mini.addWord(new WordTag("Google", "http://www.google.com"));
+        mini.addWord(new WordTag("Google", "http://www.google.com"));
+        mini.addWord(new WordTag("Google", "http://www.google.com"));
+        mini.addWord(new WordTag("Google", "http://www.google.com"));
+        mini.addWord(new WordTag("News", "http://cnn.com"));
+        mini.addWord(new WordTag("News", "http://cnn.com"));
+        mini.addWord(new WordTag("News", "http://cnn.com"));
+        mini.addImage(new ImageTag("http://lh5.ggpht.com/s/v/51.28/img/logo/pwa-fr.gif"));
+        mini.addImage(new ImageTag("http://lh5.ggpht.com/s/v/51.28/img/logo/pwa-fr.gif"));
+        mini.addImage(new ImageTag("http://lh5.ggpht.com/s/v/51.28/img/logo/pwa-fr.gif"));   
+        mini.addImage(new ImageTag("http://static.linkedin.com/img/pic/pic_logo_119x32.gif", "http://linkedin.com"));
+        
+        
+        panel.add(cloud);
+        panel.add(photoCloud);
+        panel.add(mini);
+        
+        RootPanel.get("content").add(panel);
     }
 }
